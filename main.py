0098b463b9a40690e8c97eed4b408e087188f258
Robert Schmidt <rschmidt@smu.edu>
2020-03-08 13:49:13 -0700

    Changing max sentence length to 35

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class Config():
     discriminator_method = 'Multi' # 'Multi' or 'Cond'
     load_pretrained_embed = False
     min_freq = 3
-    max_length = 30
+    max_length = 35
     embed_size = 256
     d_model = 256
     h = 4
